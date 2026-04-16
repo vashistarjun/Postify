592a6851ba3cbc30d76e5271c90d0ea2e7600a65
vashistarjun <arjunavashistexample.com>
2026-04-16 14:18:46 +0530

    Update Redis config to use environment variable for cloud deployment

--- server/config/redis.js
+++ server/config/redis.js
@@ -1,7 +1,16 @@
 const Redis = require('ioredis')
-const redis = new Redis({
-    host: "localhost",
-    port:6379
-})
 
-module.exports=redis
+let redis;
+
+if (process.env.REDIS_URL) {
+    // For cloud Redis (Render, Redis Cloud, etc.)
+    redis = new Redis(process.env.REDIS_URL);
+} else {
+    // For local development
+    redis = new Redis({
+        host: "localhost",
+        port: 6379
+    });
+}
+
+module.exports = redis;
